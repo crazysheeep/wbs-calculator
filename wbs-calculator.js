501ae93bf81bfe4b978b14d09dc9df50329c97a8
Leo Huang <crazysheep2302@gmail.com>
2013-10-12 21:41:48 +1100

    Create prototype for Project and Input pages

--- wbs-calculator.js
+++ wbs-calculator.js
@@ -1,13 +1,92 @@
+Projects = new Meteor.Collection('projects');
+LightInfo = new Meteor.Collection('lightinfo');
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to wbs-calculator.";
+  Session.setDefault('curPage', 'selectProject'); //current page - project/input etc
+  Session.setDefault('selectedProject', ''); //Project selected in project page
+  Session.setDefault('curProject', '');      //Project being worked on
+
+  Template.navbar.isActive = function (page) {
+    if (Session.equals('curPage', page)) {
+      return 'class=active';
+    }
+  };
+  Template.navbar.curProject = function () {
+    if (Session.equals('curProject', '')) {
+      return 'None';
+    }
+    return Session.get('curProject');
+  };
+  Template.navbar.events({
+    'click #navProject' : function () {
+      Session.set('curPage', 'selectProject');
+    },
+    'click #navInput' : function () {
+      Session.set('curPage', 'inputData');
+    },
+    'click #navReport' : function () {
+      Session.set('curPage', 'viewReport');
+    },
+    'click #navAnalysis' : function () {
+      Session.set('curPage', 'analysis');
+    }
+  });
+
+  Template.contentWrapper.curPage = function (page) {
+    return Session.equals('curPage', page);
   };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  Template.selectProject.projects = function () {
+    var searchTerm = '';
+    if (searchTerm) {
+      return Projects.find({name: searchTerm});
+    }
+    return Projects.find({});
+  };
+  Template.selectProject.events({
+    'click #testButton' : function () {
+      Meteor.call('dbProjectsDrop');
+    },
+    'click .projectList' : function () {
+      Session.set('selectedProject', this.code);
+    },
+    'click #confirmProject' : function () {
+      Session.set('curProject', Session.get('selectedProject'));
+      Session.set('curPage', 'inputData');
+    }
+  });
+
+  Template.inputData.curProject = function () {
+    return Projects.findOne({code: Session.get('curProject')});
+  };
+  Template.inputData.lightList = function () {
+    var projectDetails = Projects.findOne({code: this.code});
+    return projectDetails.lightList;
+  };
+  Template.inputData.lightType = function () {
+    return LightInfo.find();
+  }
+  Template.inputData.events({
+    'click #addLightBtn' : function () {
+      var location = $('#addLightLocation').val();
+      var type = $('#addLightType').val();
+      var qty = $('#addLightQty').val();
+      var hours = $('#addLightHours').val();
+      var tube = $('#addLightTube').val();
+      var aircon = $('#addLightAircon').val();
+      var sensor = $('#addLightSensor').val();
+      var newType = $('#addLightNew').val();
+      window.alert(location+type+qty+hours+tube+aircon+sensor+newType);
+      Meteor.call('dbProjectsAddLight', this.code, location, type, qty, hours, 
+                                        tube, aircon, sensor, newType);
+      $('#addLightLocation').val('');
+      $('#addLightType').val('');
+      $('#addLightQty').val('');
+      $('#addLightHours').val('');
+      $('#addLightTube').val('');
+      $('#addLightAircon').val('');
+      $('#addLightSensor').val('');
+      $('#addLightNew').val('');
     }
   });
 }
@@ -15,5 +94,36 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    if (Projects.find().count() === 0) {
+      Projects.insert({code: 'GB0001', address: '30 Felton Rd Carlingford', lightList: []});
+      Projects.insert({code: 'GB0002', address: '52 Holker St Silverwater', lightList: []});
+      Projects.insert({code: 'GB0003', address: 'some address', lightList: []});
+      Projects.insert({code: 'GB0004', address: 'al;skdfj;iosafjaw;lfj;sdio', lightList: []});
+    }
+    if (LightInfo.find().count() === 0) {
+      LightInfo.insert({type: 'EM236', price: '100.80', newType: 'EM36LED'});
+      LightInfo.insert({type: 'EM218', price: '100.80', newType: 'EM36LED'});
+      LightInfo.insert({type: '132', price: '100.80', newType: 'R15LED'});
+    }
+
+    Meteor.methods({
+      dbProjectsDrop: function () {
+        Projects.remove({});
+      },
+      dbProjectsAdd: function (code) {
+        //TODO
+      },
+      dbProjectsAddLight: function (code, location, type, qty, hours, tube, aircon, sensor, newType) {
+        Projects.update({code: code},
+                         {$push: {lightList: {location: location,
+                                              type: type,
+                                              qty: qty,
+                                              hours: hours,
+                                              tube: tube,
+                                              aircon: aircon,
+                                              sensor: sensor,
+                                              newType: newType}}});
+      }
+    });
   });
 }
